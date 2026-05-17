9430e6928e4f263aed2edb1d37261d1f0f15277b
2DimBer3 <berezan4@mail.ru>
2026-05-17 13:27:59 +0300

    fix DesignForExtension

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -3,12 +3,21 @@ package hexlet.code.schemas;
 public abstract class BaseSchema<T> {
     protected boolean required = false;
 
+    /**
+     * Делает значение обязательным (не допускается пустое значение).
+     *
+     * <p>Подклассы могут переопределять этот метод для поддержки fluent-интерфейса,
+     * возвращая конкретный тип схемы. При переопределении обязательно вызвать
+     * {@code super.required()}.
+     *
+     * @return this (или экземпляр подкласса)
+     */
     public BaseSchema<T> required() {
         this.required = true;
         return this;
     }
 
-    public boolean isValid(T value) {
+    public final boolean isValid(T value) {
         if (isNullValue(value)) {
             return !required;
         }
